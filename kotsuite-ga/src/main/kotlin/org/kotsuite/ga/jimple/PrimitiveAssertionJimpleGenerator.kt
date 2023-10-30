98e84f3c13a761e575f11027ebd708109c3553db
yangfeng <1719957182@qq.com>
2023-10-30 20:01:59 +0800

    refactor: PrimitiveAssertionJimpleGenerator part

--- kotsuite-ga/src/main/kotlin/org/kotsuite/ga/jimple/PrimitiveAssertionJimpleGenerator.kt
+++ kotsuite-ga/src/main/kotlin/org/kotsuite/ga/jimple/PrimitiveAssertionJimpleGenerator.kt
@@ -31,40 +31,58 @@ object PrimitiveAssertionJimpleGenerator {
     private fun createAssertStatement(body: Body, assertType: String, assertValue: String, actualValue: Local) {
         val assertTypeJavaClass = getJavaClass(assertType)
         val expectedValue = string2Value(assertValue, assertTypeJavaClass)
+        val className: String
+        val constructorMethodSig: String
 
-        val expectedObject = when (assertTypeJavaClass) {
+        when (assertTypeJavaClass) {
             "java.lang.Boolean" -> {
-                createExpectedObject(body, expectedValue, PrimitiveConstants.boolean_class_name, PrimitiveConstants.boolean_constructor_method_sig)
+                className = PrimitiveConstants.boolean_class_name
+                constructorMethodSig = PrimitiveConstants.boolean_constructor_method_sig
             }
             "java.lang.Byte" -> {
-                createExpectedObject(body, expectedValue, PrimitiveConstants.byte_class_name, PrimitiveConstants.byte_constructor_method_sig)
+                className = PrimitiveConstants.byte_class_name
+                constructorMethodSig = PrimitiveConstants.byte_constructor_method_sig
             }
             "java.lang.Character" -> {
-                createExpectedObject(body, expectedValue, PrimitiveConstants.character_class_name, PrimitiveConstants.character_constructor_method_sig)
+                className = PrimitiveConstants.character_class_name
+                constructorMethodSig = PrimitiveConstants.character_constructor_method_sig
             }
             "java.lang.Double" -> {
-                createExpectedObject(body, expectedValue, PrimitiveConstants.double_class_name, PrimitiveConstants.double_constructor_method_sig)
+                className = PrimitiveConstants.double_class_name
+                constructorMethodSig = PrimitiveConstants.double_constructor_method_sig
             }
             "java.lang.Float" -> {
-                createExpectedObject(body, expectedValue, PrimitiveConstants.float_class_name, PrimitiveConstants.float_constructor_method_sig)
+                className = PrimitiveConstants.float_class_name
+                constructorMethodSig = PrimitiveConstants.float_constructor_method_sig
             }
             "java.lang.Integer" -> {
-                createExpectedObject(body, expectedValue, PrimitiveConstants.integer_class_name, PrimitiveConstants.integer_constructor_method_sig)
+                className = PrimitiveConstants.integer_class_name
+                constructorMethodSig = PrimitiveConstants.integer_constructor_method_sig
             }
             "java.lang.Long" -> {
-                createExpectedObject(body, expectedValue, PrimitiveConstants.long_class_name, PrimitiveConstants.long_constructor_method_sig)
+                className = PrimitiveConstants.long_class_name
+                constructorMethodSig = PrimitiveConstants.long_constructor_method_sig
             }
             "java.lang.String" -> {
-                createExpectedObject(body, expectedValue, PrimitiveConstants.string_class_name, PrimitiveConstants.string_constructor_method_sig)
+                className = PrimitiveConstants.string_class_name
+                constructorMethodSig = PrimitiveConstants.string_constructor_method_sig
             }
             else -> {
                 throw Exception("Unsupported assert type: $assertTypeJavaClass")
             }
         }
 
+        val expectedObject = createExpectedObject(body, expectedValue, className, constructorMethodSig)
         createAssertionStatement(body, expectedObject, actualValue)
     }
 
+    /**
+     * Transform `assertValue` (`String`) to `soot.Value`
+     *
+     * @param assertValue the expected value in assert statement (`string`)
+     * @param assertTypeJavaClass the type of the expected value
+     * @return the expected value in assert statement (`soot.Value`)
+     */
     private fun string2Value(assertValue: String, assertTypeJavaClass: String): Value {
         return when (assertTypeJavaClass) {
             "java.lang.Boolean" -> {
@@ -87,6 +105,15 @@ object PrimitiveAssertionJimpleGenerator {
         }
     }
 
+    /**
+     * Create expected object in assert statement
+     *
+     * @param body the method body which needs to generate expected object
+     * @param expectedValue the expected value in assert statement (`soot.Value`)
+     * @param expectedObjectClassName the class name of the expected object
+     * @param expectedObjectClassConstructorSig
+     * @return
+     */
     private fun createExpectedObject(
         body: Body,
         expectedValue: Value,
