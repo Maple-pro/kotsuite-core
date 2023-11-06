41612449c3662a639394d222d8e2cc627589da46
yangfeng <1719957182@qq.com>
2023-11-06 22:50:42 +0800

    feat: add isUnsupportedMethod filter
    Add comments in Filter

--- kotsuite-common/src/main/kotlin/org/kotsuite/soot/Filter.kt
+++ kotsuite-common/src/main/kotlin/org/kotsuite/soot/Filter.kt
@@ -8,25 +8,26 @@ import soot.SootMethod
 
 object Filter {
     fun testSuiteGeneratorClassFilter(sootClass: SootClass): Boolean {
-        return !sootClass.isAnonymousClass()
-                && sootClass.isPublicClass()
-                && !sootClass.isAbstractClass()
-                && !sootClass.isInterfaceClass()
-                && !sootClass.isObject()
-                && !sootClass.isDataClass()
+        return !sootClass.isAnonymousClass()                // 该类是否是匿名类
+                && sootClass.isPublicClass()                // 该类是否是公共类
+                && !sootClass.isAbstractClass()             // 该类是否是抽象类
+                && !sootClass.isInterfaceClass()            // 该类是否是接口
+                && !sootClass.isObject()                    // 该类是否是 Object
+                && !sootClass.isDataClass()                 // 该类是否是数据类
 //                && !sootClass.isAndroidClass()
     }
 
     fun testSuiteGeneratorMethodFilter(sootMethod: SootMethod): Boolean {
         return sootMethod.isPublicMethod()
-                && !sootMethod.isConstructorMethod()
-                && !sootMethod.isAnonymousMethod()
-                && !sootMethod.isAbstractMethod()
+                && !sootMethod.isConstructorMethod()        // 该函数是否是构造函数
+                && !sootMethod.isAnonymousMethod()          // 该函数是否是匿名函数
+                && !sootMethod.isAbstractMethod()           // 该函数是否是抽象函数
 //                && !sootMethod.isAndroidParameterMethod()
-                && !sootMethod.isGetSetMethod()
+                && !sootMethod.isGetSetMethod()             // 该函数是否是 Get/Set 函数
 //                && !sootMethod.isObjectMethod()
-                && !sootMethod.isComponentNMethod()
-                && !sootMethod.isOverrideMethod()
+                && !sootMethod.isComponentNMethod()         // 该函数是否是 componentN 函数
+                && !sootMethod.isOverrideMethod()           // 该函数是否是 Android 类中函数的重载, e.g., onCreate()
+                && !sootMethod.isUnsupportedMethod()        // 函数参数中是否包含未支持的类型
     }
 
     private fun SootMethod.isConstructorMethod(): Boolean {
@@ -182,5 +183,13 @@ object Filter {
         }
 
         return results
+    }
+
+    private fun SootMethod.isUnsupportedMethod(): Boolean {
+        val parameters = this.parameterTypes
+        return parameters
+            .filterIsInstance<RefType>().any {
+                it.className.startsWith("kotlin.jvm.functions")
+            }
     }
 }
