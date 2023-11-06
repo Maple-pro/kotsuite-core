471bd28fcbf949056d58f194e5ad2c2469e13db3
yangfeng <1719957182@qq.com>
2023-11-06 22:51:09 +0800

    feat: add more log

--- kotsuite-ga/src/main/kotlin/org/kotsuite/ga/coverage/fitness/PopulationFitness.kt
+++ kotsuite-ga/src/main/kotlin/org/kotsuite/ga/coverage/fitness/PopulationFitness.kt
@@ -1,5 +1,6 @@
 package org.kotsuite.ga.coverage.fitness
 
+import org.apache.logging.log4j.LogManager
 import org.kotsuite.Configs
 import org.kotsuite.ga.chromosome.Population
 import org.kotsuite.ga.printer.JasminPrinter
@@ -10,18 +11,22 @@ import soot.SootClass
 import soot.SootMethod
 
 object PopulationFitness {
+    private val log = LogManager.getLogger()
 
     fun generatePopulationFitness(population: Population, assertFilePath: String) {
         // Generate jimple test class
+        log.debug("a. Generating Jimple test classes...")
         val jimpleTestClass = population.generateJimpleTestClass()
 
         // Print main class and test classes into file using jasmin format
+        log.debug("b. Printing Jasmin files...")
         JasminPrinter.printJasminFile(jimpleTestClass)
 
         // Generate fitness for each test cases
         population.testCases.forEach { testCase ->
             // Run application with jacoco agent and kotsuite agent, which will generate the .exec file
             // Analyze the .exec file to generate the coverage information
+            log.debug("c. Generating fitness for test case: ${testCase.testCaseName}")
             TestCaseFitness(
                 jimpleTestClass,
                 testCase,
@@ -31,6 +36,7 @@ object PopulationFitness {
         }
 
         // Generate fitness value for whole population
+        log.debug("d. Generating fitness for whole population")
         val execDataFile = Configs.getExecFilePath(jimpleTestClass.name, "*")
         generateTotalPopulationExec(
             execDataFile,
